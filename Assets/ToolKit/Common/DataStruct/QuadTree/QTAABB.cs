015a06167c0be30a6ac76094ca4b0b3b3e660512
unknown <tongguangdong@q1oa.com>
2025-01-01 17:37:18 +0800

    提交代码

---
+++ Assets/ToolKit/Common/DataStruct/QuadTree/QTAABB.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Mathematics;
+
+namespace ToolKit.Common.DataStruct
+{
+    public class QTAABB
+    {
+        public float2 Min { get; set; }
+        public float2 Max { get; set; }
+        public float2 Center => (Min + Max) / 2;
+
+        public QTAABB(float2 min, float2 max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
