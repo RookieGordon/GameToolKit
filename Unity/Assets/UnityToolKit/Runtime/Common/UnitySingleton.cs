2815b0723d493838d672573ab20030d1d3d98b68
guangdong <guangdong93@outlook.com>
2026-06-30 18:06:31 +0800

    测试用例整理

---
+++ Unity/Assets/UnityToolKit/Runtime/Common/UnitySingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityToolKit.Runtime.Common
+{
+    public class UnitySingleton<T>: MonoBehaviour where T : Component
+    {
+        private static GameObject _singletonRoot;
+        
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_singletonRoot == null)
+                {
+                    _singletonRoot = new GameObject("SingletonRoot");
+                    DontDestroyOnLoad(_singletonRoot);
+                }
+
+                if (_instance == null)
+                {
+                    _instance = _singletonRoot.transform.GetComponentInChildren<T>();
+                    if (_instance == null)
+                    {
+                        var obj = new GameObject($"{typeof(T).Name}");
+                        DontDestroyOnLoad(obj);
+                        obj.transform.SetParent(_singletonRoot.transform);
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
